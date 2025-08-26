78bfcec4a5196bcbc875a9fab765ad9896f7517a
zyadelhady <zyade40@gmail.com>
2025-08-27 00:26:39 +0300

    use db url from .env

--- backend/database.py
+++ backend/database.py
@@ -7,7 +7,7 @@ import datetime
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/postgres")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
